d93e3045a271a179cbfde8a77e60b8cdac4eede5
Berk-54 <9016037@student.zadkine.nl>
2024-10-11 13:23:09 +0200

    Add files via upload

---
+++ opdracht 6.2/script.js
@@ -0,0 +1,11 @@
+function kleur_aanpassen() {
+    const titel = document.getElementById('titel');
+    
+    titel.style.color = 'green';
+    titel.textContent = 'Inhoud aangepast! naar de kleur GROEN'; 
+    
+    titel.style.color = 'red';
+    titel.textContent= 'Inhoud aangepast! naar de kleur ROOD';
+
+
+}
