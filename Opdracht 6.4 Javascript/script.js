a4fb427cf0e0c802202ffd58233dd52941606639
Berk-54 <9016037@student.zadkine.nl>
2024-11-08 15:16:15 +0100

    Add files via upload

---
+++ Opdracht 6.4 Javascript/script.js
@@ -0,0 +1,20 @@
+
+function calculate(num1, num2) {
+    let sum = num1 + num2;
+    console.log(sum)
+}
+calculate(1,12);
+
+function multiplyByFive(num) {
+    let product = num * 5;
+    console.log(product)
+}
+
+
+multiplyByFive(7); 
+
+function calculateMinutes(seconds) {
+    let minutes = seconds / 60;
+    console.log(minutes)
+}
+calculateMinutes(3600); 
