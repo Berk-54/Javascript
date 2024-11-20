319e26e80bc8784d77351979cb8df37a92805498
Berk-54 <9016037@student.zadkine.nl>
2024-11-20 11:49:09 +0100

    Add files via upload

---
+++ opdracht 8.2 Javascript/script.js
@@ -0,0 +1,53 @@
+function Sum() {
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+
+    
+    if (num1 <= 0 || num2 <= 0) {
+        document.getElementById('result').innerHTML = "Fout: Het getal is te laag!";
+    } else {
+        var result = num1 + num2;
+        document.getElementById('result').innerHTML = "Resultaat: " + result;
+    }
+}
+
+function Minus() {
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+
+    
+    if (num1 <= 0 || num2 <= 0) {
+        document.getElementById('result').innerHTML = "Fout: Het getal is te laag!";
+    } else {
+        var result = num1 - num2;
+        document.getElementById('result').innerHTML = "Resultaat: " + result;
+    }
+}
+
+function Multiply() {
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+
+    
+    if (num1 <= 0 || num2 <= 0) {
+        document.getElementById('result').innerHTML = "Fout: Het getal is te laag!";
+    } else {
+        var result = num1 * num2;
+        document.getElementById('result').innerHTML = "Resultaat: " + result;
+    }
+}
+
+function Divide() {
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+
+    
+    if (num1 <= 0 || num2 <= 0) {
+        document.getElementById('result').innerHTML = "Fout: Het getal is te laag!";
+    } else if (num2 === 0) {
+        document.getElementById('result').innerHTML = "Fout: Delen door nul is niet toegestaan!";
+    } else {
+        var result = num1 / num2;
+        document.getElementById('result').innerHTML = "Resultaat: " + result;
+    }
+}
